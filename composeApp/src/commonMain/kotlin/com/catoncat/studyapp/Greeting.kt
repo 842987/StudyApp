59c08d1809d22c31ad4b38fb6fc2d627cacb4432
842987 <pavlov.albert.cat@yandex.ru>
2026-03-17 23:07:28 +0700

    Добавлена навигация, экраны

--- composeApp/src/commonMain/kotlin/com/catoncat/studyapp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.catoncat.studyapp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/catoncat/studyapp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.catoncat.studyapp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
